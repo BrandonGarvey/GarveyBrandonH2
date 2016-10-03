5c27fb488b7e56a74a2f85e963d46922ca0f2ce5
Brandon <brandongarvey@Brandons-MacBook-Air.local>
2016-10-02 20:35:38 -0400

    adds main method

---
+++ babylonianmethod.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class babylonianmethod {
+  
+    public static void main(String[] args)
+    {
+        double result = getSqrt(Double.parseDouble(args[0]));
+        System.out.println("Take the square root of: ");
+        System.out.println("Answer: " + result);
+    }
+    
